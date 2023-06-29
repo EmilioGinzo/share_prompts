e10deeb2d46ddfa8ecabbdaff546f1a7dd2e93a2
Emilio Ginzo <68378278+EmilioGinzo@users.noreply.github.com>
2023-06-29 00:43:35 -0400

    Now Sign In with google auth is working

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -3,22 +3,22 @@
 import Link from "next/link";
 import Image from "next/image";
 import { useState, useEffect } from "react";
-import { signIn, signOut, userSession, getProviders } from "next-auth/react";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
 
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
 
   useEffect(() => {
-    const setProviders = async () => {
+    const setUpProviders = async () => {
       const response = await getProviders();
 
       setProviders(response);
     }
 
-    setProviders();
+    setUpProviders();
   }, []);
 
   return (
@@ -36,7 +36,7 @@ const Nav = () => {
 
       {/* Desktop Navigation */}
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt"
             className="black_btn">
@@ -50,7 +50,7 @@ const Nav = () => {
 
             <Link href="/profile">
               <Image
-                src="/assets/images/logo.svg"
+                src={session?.user.image}
                 width={37}
                 height={37}
                 className="rounded-full"
@@ -61,7 +61,7 @@ const Nav = () => {
         ): (
           <>
             {providers &&
-              Object.values(providers).map(() => (
+              Object.values(providers).map((provider) => (
                 <button
                   type="button"
                   key={provider.name}
@@ -77,10 +77,10 @@ const Nav = () => {
 
       { /* Mobile Navigation */}
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex">
             <Image
-              src="/assets/images/logo.svg"
+              src={session?.user.image}
               width={37}
               height={37}
               className="rounded-full"
@@ -120,7 +120,7 @@ const Nav = () => {
         ): (
           <>
             {providers &&
-              Object.values(providers).map(() => (
+              Object.values(providers).map((provider) => (
                 <button
                   type="button"
                   key={provider.name}
