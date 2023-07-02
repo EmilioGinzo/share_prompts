b37a8e7ccedd9ee896f608b80702024f9379fbfd
Emilio Ginzo <emilioginzo@fpuna.edu.py>
2023-07-02 17:34:37 -0400

    Now the connection with the DB is working

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,49 +1,54 @@
-import NextAuth from "next-auth/next";
+import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 import User from "@models/user";
 import { connectToDB } from "@utils/database";
 
 const handler = NextAuth({
+  // Configure one or more authentication providers
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET
     })
   ],
-  async session({ session }) {
-    const sessionUser = await User.findOne({
-      email: session.user.email
-    });
+  callbacks: {
+    async session({ session }) {
 
-    session.user.id = sessionUser._id.toString();
-
-    return session;
-  },
-  async singIn({ profile }){
-    try {
-      await connectToDB();
-
-      // check if a user already exists
-      const userExists = await User.findOne({
-        email: profile.email
+      const sessionUser = await User.findOne({
+        email: session.user.email
       });
+      session.user.id = sessionUser._id.toString();
 
-      // if not, create a new user
-      if(!userExists) {
-        await User.create({
-          email: profile.email,
-          username: profile.name.replace(" ", "").toLowerCase(),
-          image: profile.picture
+      return session;
+    },
+
+    async singIn({  account, profile, user, credentials }){
+      try {
+
+        await connectToDB();
+
+        // check if a user already exists
+        const userExists = await User.findOne({
+          email: profile.email
         });
-      }
 
-      return true;
-    } catch (error) {
-      console.log(error);
-      return false;
+        // if not, create a new user
+        if(!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          });
+        }
+
+        return true;
+      } catch (error) {
+        console.log(error.message);
+        return false;
+      }
     }
   }
 })
 
-export {handler as GET, handler as POST};
+export { handler as GET, handler as POST };
